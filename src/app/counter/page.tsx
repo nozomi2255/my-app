43d7fc48b9aa6cd8d10c48fb39c8b005281bd641
Nozomi <nozomi.tomita.uhd@gmail.com>
2025-03-11 13:43:27 +0900

    API routeの作成、useEffectテスト

--- src/app/counter/page.tsx
+++ src/app/counter/page.tsx
@@ -1,13 +1,33 @@
 "use client";
 import { useState } from 'react';
 
-export default function CounterPage() {
+// 子コンポーネント
+type CounterProps = {
+    message: string;
+};
+
+function Counter({ message }: CounterProps) {
   const [count, setCount] = useState(0);
 
   return (
     <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h2>{message}</h2>
       <h1>カウンター: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button 
+        className="bg-blue-500 text-white px-4 py-2"
+        onClick={() => setCount(count + 1)}
+      >
+        +1
+      </button>
+    </div>
+  );
+};
+
+// 親コンポーネント
+export default function CounterPage() {
+  return (
+    <div className="p-8">
+      <Counter message="Hello Counter" />
     </div>
   );
 }
